7081913eb4ae6a553f5599c90c9b70ac166be95e
Joziah.Walters <joiahwalters70@gmail.com>
2025-02-04 12:00:54 +0100

    Consolidate flight and plane routes into a single api.php file

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,21 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\PlaneController;
+use App\Http\Controllers\FlightController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
+
+Route::get('/planes',[PlaneController::class, 'index'])->name('apiHome');
+Route::get('/planes/{id}',[PlaneController::class, 'show'])->name('apiShow');
+Route::post('/planes',[PlaneController::class, 'store'])->name('apiStore');
+Route::put('/planes/{id}',[PlaneController::class, 'update'])->name('apiUpdate');
+Route::delete('/planes/{id}',[PlaneController::class, 'destroy'])->name('apiDestroy');
+
+Route::get('/flight',[FlightController::class, 'index'])->name('apiHome');
+Route::get('/flight/{id}',[FlightController::class, 'show'])->name('apiShow');
+Route::post('/flight',[FlightController::class, 'store'])->name('apiStore');
+Route::put('/flight/{id}',[FlightController::class, 'update'])->name('apiUpdate');
+Route::delete('/flight/{id}',[FlightController::class, 'destroy'])->name('apiDestroy');
