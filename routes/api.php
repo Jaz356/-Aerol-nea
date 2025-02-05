5b3cc999ab148d89367b08350250a3b12fd6d32c
Joziah.Walters <joiahwalters70@gmail.com>
2025-02-05 09:38:24 +0100

    Refactor API routes for planes and flights with more descriptive names; remove unused feature tests

--- routes/api.php
+++ routes/api.php
@@ -1,22 +1,22 @@
 <?php
 
+use FlightController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PlaneController;
-use App\Http\Controllers\FlightController;
 
 Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::get('/planes',[PlaneController::class, 'index'])->name('apiHome');
-Route::get('/planes/{id}',[PlaneController::class, 'show'])->name('apiShow');
-Route::post('/planes',[PlaneController::class, 'store'])->name('apiStore');
-Route::put('/planes/{id}',[PlaneController::class, 'update'])->name('apiUpdate');
-Route::delete('/planes/{id}',[PlaneController::class, 'destroy'])->name('apiDestroy');
+Route::get('/planes',[PlaneController::class, 'index'])->name('apiPlaneHome');
+Route::get('/planes/{id}',[PlaneController::class, 'show'])->name('apiPlaneShow');
+Route::post('/planes',[PlaneController::class, 'store'])->name('apiPlaneStore');
+Route::put('/planes/{id}',[PlaneController::class, 'update'])->name('apiPlaneUpdate');
+Route::delete('/planes/{id}',[PlaneController::class, 'destroy'])->name('apiPlaneDestroy');
 
-Route::get('/flight',[FlightController::class, 'index'])->name('apiHome');
-Route::get('/flight/{id}',[FlightController::class, 'show'])->name('apiShow');
-Route::post('/flight',[FlightController::class, 'store'])->name('apiStore');
-Route::put('/flight/{id}',[FlightController::class, 'update'])->name('apiUpdate');
-Route::delete('/flight/{id}',[FlightController::class, 'destroy'])->name('apiDestroy');
+Route::get('/flight',[FlightController::class, 'index'])->name('apiFlightHome');
+Route::get('/flight/{id}',[FlightController::class, 'show'])->name('apiFlightShow');
+Route::post('/flight',[FlightController::class, 'store'])->name('apiFlightStore');
+Route::put('/flight/{id}',[FlightController::class, 'update'])->name('apiFlightUpdate');
+Route::delete('/flight/{id}',[FlightController::class, 'destroy'])->name('apiFlightDestroy');
